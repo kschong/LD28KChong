49c7f88d1c410c179a966f720fdbf38a624d3b3a
kschong <kenneth.s.chong@gmail.com>
2013-12-13 18:41:27 -0800

    Third party library SFML added to project.

--- LD28KChong/main.cpp
+++ LD28KChong/main.cpp
@@ -1,3 +1,5 @@
+#include <SFML\Graphics.hpp>
+
 #define SCREEN_WIDTH	1024
 #define SCREEN_HEIGHT	768
 #define BITS_PER_PIXEL	32
@@ -5,4 +7,39 @@
 
 int main()
 {
+	sf::ContextSettings settings;
+	settings.antialiasingLevel = ANTIALIAS_LEVEL;
+	sf::RenderWindow Window = sf::RenderWindow(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, BITS_PER_PIXEL), "LD28KChong",  sf::Style::Default, settings);
+
+	// used to compute fps
+	sf::Clock clock;
+	sf::Time lElapsedTime = clock.getElapsedTime();
+	sf::Time fpsTime = clock.getElapsedTime();				
+	int numFrames = 0;
+
+	sf::Event Event;
+
+	while (Window.isOpen())
+	{
+		while (Window.pollEvent(Event))
+		{
+			switch (Event.type)
+			{
+			case sf::Event::Closed:
+				Window.close();
+				break;
+			}
+
+			float lFrameTime = clock.getElapsedTime().asSeconds() - lElapsedTime.asSeconds();
+			lElapsedTime = clock.getElapsedTime();
+
+			if(lFrameTime >1)
+				lFrameTime =0;
+
+			Window.clear();
+			Window.display();
+		}
+	}
+
+	return 0;
 }
